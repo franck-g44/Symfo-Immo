64e812de676982e3ffbe68bdfa042f0514fca302
franck-g44 <franck44250@gmail.com>
2019-10-28 16:44:02 +0100

    page acheter, modification des routes

--- Symfo-Immo/src/Controller/PropertyController.php
+++ Symfo-Immo/src/Controller/PropertyController.php
@@ -5,19 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
-class HomeController extends AbstractController
-{
-    private $render;
+class PropertyController extends AbstractController {
 
     /**
-     * @Route("/", name="home")
+     * @Route("/biens", name="property.index.html.twig")
      * @return Response
      */
 
     public function index(): Response
     {
-    return $this->render('pages/home.html.twig');
+        return $this->render('property/index.html.twig');
     }
 }
