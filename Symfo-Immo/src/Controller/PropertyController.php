4100ac68d1e12565303502348dd95add131669c6
franck-g44 <franck44250@gmail.com>
2019-10-29 16:23:39 +0100

    BDD + entity + repository

--- Symfo-Immo/src/Controller/PropertyController.php
+++ Symfo-Immo/src/Controller/PropertyController.php
@@ -2,11 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PropertyController extends AbstractController {
+class PropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository)
+    {
+
+        $this->repository = $repository;
+    }
 
     /**
      * @Route("/biens", name="property.index.html.twig")
@@ -15,6 +28,8 @@ class PropertyController extends AbstractController {
 
     public function index(): Response
     {
-        return $this->render('property/index.html.twig');
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties'
+        ]);
     }
 }
